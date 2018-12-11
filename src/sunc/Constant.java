5ed82e70d782daf9066328961f339658728baf7d
sunc <sunc@openscanner.cn>
2018-12-11 18:34:09 +0800

    my

---
+++ src/sunc/Constant.java
@@ -0,0 +1,9 @@
+package sunc;
+
+/**
+ * creator: sunc date: 2018/12/11 description:
+ */
+public class Constant {
+
+    public static final String imagePath = System.getProperty("user.dir") + "/source/a.jpg";
+}
